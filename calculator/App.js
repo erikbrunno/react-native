62665b7873ca5f8ee381a3c7a4a7634de6fd8cb1
Erik Brunno <erik.brunno@yahoo.com.br>
2019-03-08 14:34:13 -0300

    resolver bugs calculadora

--- calculator/App.js
+++ calculator/App.js
@@ -18,12 +18,13 @@ export default class App extends Component {
   }
 
   addDigit = n => {
-    if (n === '.' && this.state.displayValue.includes('.')) {
-      return
-    }
-
     const clearDisplay = this.state.displayValue === '0'
       || this.state.clearDisplay
+
+      if (n === '.' && !clearDisplay 
+          && this.state.displayValue.includes('.')) {
+        return
+      }
 
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
@@ -56,7 +57,7 @@ export default class App extends Component {
 
       values[1] = 0
       this.setState({
-        displayValue: values[0],
+        displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
         clearDisplay: true,
