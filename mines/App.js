5bf74e46e823b8ab9603a50489cda7d7e341f260
erikbrunno <erik.brunno@yahoo.com.br>
2019-03-12 05:07:37 -0300

    criar componente field campo minado

--- mines/App.js
+++ mines/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
+import Field from './src/components/Field'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Iniciando com Mines: </Text>
         <Text>Tamanho da grade: 
           {params.getRowsAmount()} X {params.getColumnsAmount()}</Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={6}/>
       </View>
     );
   }
@@ -17,8 +24,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    
   },
 });
