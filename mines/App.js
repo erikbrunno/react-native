9a8557f8562dcf8b8ff0010cbc51a748a288bf0b
Erik Brunno <erik.brunno@yahoo.com.br>
2019-03-13 10:20:59 -0300

    componente minefield

--- mines/App.js
+++ mines/App.js
@@ -2,25 +2,37 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
 import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import { createMineBoard } from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return {
+      board: createMineBoard(rows, cols, this.minesAmount())
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Iniciando com Mines: </Text>
-        <Text>Tamanho da grade: 
-          {params.getRowsAmount()} X {params.getColumnsAmount()}</Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1}/>
-        <Field opened nearMines={2}/>
-        <Field opened nearMines={3}/>
-        <Field opened nearMines={6}/>
-        <Field mined />
-        <Field mined opened/>
-        <Field mined opened exploded/>
-        <Field flagged/>
-        <Field flagged opened/>
+        <View style={styles.board}>
+          <MineField board={this.state.board}></MineField>
+        </View>
       </View>
     );
   }
@@ -29,10 +41,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    
+    justifyContent: 'flex-end',
   },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
 });
--- mines/src/components/mineField.js
+++ mines/src/components/mineField.js
@@ -3,5 +3,19 @@ import { View, StyleSheet } from 'react-native'
 import Field from './Field'
 
 export default props => {
-    
+    const row = props.board.map((row, r) => {
+        const columns = row.map((field, c) => {
+            return <Field {...field} key={c}></Field>
+        })
+
+        return <View key={r}>{columns}</View>
+    })
+    return <View style={styles.container}>{row}</View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        backgroundColor: '#EEE'
+    }
+})
