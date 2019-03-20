3726b895ccabbc88861daeeba05cdb9632563ace
Erik Brunno <erik.brunno@yahoo.com.br>
2019-03-20 11:11:40 -0300

    ajuste componente nivel dificuldade

--- mines/src/screens/LevelSelection.js
+++ mines/src/screens/LevelSelection.js
@@ -41,7 +41,7 @@ const styles =StyleSheet.create({
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'rgba(0,0,0,6)',
+        backgroundColor: 'rgba(0,0,0,0)',
     },
     container: {
         backgroundColor: '#EEE',
