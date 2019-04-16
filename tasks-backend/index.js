87174938e768df6f372720c3d2a552bd1f1ea71d
erikbrunno <erik.brunno@yahoo.com.br>
2019-04-16 04:05:35 -0300

    backend

--- tasks-backend/index.js
+++ tasks-backend/index.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-
-    res.status(200).send('Backend iniciado')
-})
-
 app.listen(3000, () => {
     console.log('Backend executando ...')
 })
