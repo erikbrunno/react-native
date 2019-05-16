5f76baaad84f004ae7fc9f7e5038d3e4289dd6ff
Erik Brunno <erik.brunno@yahoo.com.br>
2019-05-16 10:38:53 -0300

    create menu using gravatar

--- tasks/src/Navigator.js
+++ tasks/src/Navigator.js
@@ -3,6 +3,7 @@ import {
     createSwitchNavigator, 
     createDrawerNavigator 
 } from 'react-navigation'
+import Menu from './screens/Menu'
 import Agenda from './screens/Agenda'
 import Auth from './screens/Auth'
 
@@ -43,6 +44,7 @@ const MenuRoutes ={
 
 const MenuConfig = {
     initialRouteName: 'Today',
+    contentComponent: Menu,
     contentOptions: {
         labelStyle: {
             fontWeight: 'normal',
--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -49,7 +49,7 @@ export default class Auth extends Component {
             axios.defaults.headers.common['Authorization']
                 = `bearer ${res.data.token}`
                 
-            this.props.navigation.navigate('Home')
+            this.props.navigation.navigate('Home', res.data)
         } catch (err) {
 
             Alert.alert('Erro', 'Falha no Login!')
