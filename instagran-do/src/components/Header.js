8642912a4ccd4f833b10f3c444415cb763208e6b
Erik Brunno <erik.brunno@yahoo.com.br>
2019-05-29 12:03:38 -0300

    create new project

---
+++ instagran-do/src/components/Header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import  {
+    StyleSheet,
+    Text,
+    View,
+    Platform,
+    Image
+} from 'react-native'
+import icon from '../../assets/imgs/icon.png'
+
+class Header extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.rowContainer}>
+                    <Image source={icon} style={styles.image} />
+                    <Text style={styles.title}>Instagrando</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Platform.OS === 'ios' ? 20 : 15,
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#BBB'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    image: {
+        height: 32,
+        width: 32,
+        resizeMode: 'contain'
+    },
+    title: {
+        color: '#000',
+        height: 32,
+        fontSize: 25
+    }
+})
+
+export default Header
---
+++ instagran-do/src/components/Post.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import  {
+    StyleSheet,
+    View,
+    Image,
+    Dimensions
+} from 'react-native'
+import Author from './Author'
+
+class Post extends Component {
+    render() {
+        return(
+            <View style={styles.container}>
+                <Image source={this.props.image} style={styles.image} />
+                <Author email='erik.brunno@gmail.com' nickname='Fulano de tal' />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width * 3 / 4,
+        resizeMode: 'contain'
+    }
+})
+
+export default Post
