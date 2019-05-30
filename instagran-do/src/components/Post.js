6db98327c22a3285e24633343d9c20833f26383d
Erik Brunno <erik.brunno@yahoo.com.br>
2019-05-30 11:49:32 -0300

    create component comment

--- instagran-do/src/components/Post.js
+++ instagran-do/src/components/Post.js
@@ -6,13 +6,26 @@ import  {
     Dimensions
 } from 'react-native'
 import Author from './Author'
+import Comments from './Comments'
+import AddComment from './AddComment'
 
 class Post extends Component {
     render() {
+
+        const comments = [{
+            nickname: 'João Eleno Souza',
+            comment: 'Excelente foto'
+        }, {
+            nickname: 'Maria jose',
+            comment: 'Muito boa a foto'
+        }]
+
         return(
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image} />
                 <Author email='erik.brunno@gmail.com' nickname='Fulano de tal' />
+                <Comments comments={comments} />
+                <AddComment />
             </View>
         )
     }
